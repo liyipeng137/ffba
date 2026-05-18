47f736a80cda2cdc30b7b4b35c26a366cddae227
liyipeng137 <2394304963@qq.com>
2026-05-18 11:06:17 +0800

    d

--- MERG3R/algos/dense_debug.py
+++ MERG3R/algos/dense_debug.py
@@ -260,7 +260,7 @@ def build_sparse_dense_anchor_diagnostics(predictions, images, track, points_id,
     return observations, frame_observations
 
 
-def export_dense_debug_outputs(
+def d(
     output_dir,
     predictions,
     images,
